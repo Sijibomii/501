c38369203df2c7d09872e73351bf094f45ac078d
Jaybee020 <69249201+Jaybee020@users.noreply.github.com>
2024-08-08 23:23:31 +0100

    Update config.ts

--- server/config.ts
+++ server/config.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = Number(process.env.PORT || 8000);
-export const REDIS_URL = Number(process.env.REDIS_URL || 8000);
+export const REDIS_URL = process.env.REDIS_URL;
 export const uri = String(process.env.MONGO_URI);
 export const SERVER_API_KEY = String(process.env.SERVER_API_KEY);
 export const ADMIN_API_KEY = String(process.env.ADMIN_API_KEY);
